e363d1ddfba5f4ec4fb70034f8390ee59a58d581
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-12 23:50:37 +0700

    update feedback/plugins/security.py

--- feedback/plugins/security.py
+++ feedback/plugins/security.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from pyrogram.raw import functions
 from pyrogram.types import Message
 
-from utils.database import db, prefix
+from utils.database import db
 from utils.helpers.filters import(
     pmstatus,
     contacts,
@@ -52,7 +52,7 @@ async def _antipm_(client: Client, message: Message):
     )
 
 
-@Client.on_message(filters.command("antipm", prefix) & filters.me)
+@Client.on_message(filters.command("antipm") & filters.me)
 async def _antipm(_, message: Message):
     if len(message.command) == 1:
         if db.get("core.antipm", "status", False):
@@ -79,55 +79,3 @@ async def _antipm(_, message: Message):
         )
 
 
-@Client.on_message(filters.command("pmreport", prefix) & filters.me)
-async def _report(_, message: Message):
-    if len(message.command) == 1:
-        if db.get("core.antipm", "pmreport", False):
-            await message.edit(
-                "PM-Report Status: ON!\n"
-                f"Deactivated: <code>{prefix}pmreport off</code>"
-            )
-        else:
-            await message.edit(
-                "PM-Report OFF!\n"
-                f"Activated: <code>{prefix}pmreport on</code>"
-            )
-    elif message.command[1] == "on":
-        db.set("core.antipm", "pmreport", True)
-        await message.edit("PM-Report ON!")
-    elif message.command[1] == "off":
-        db.set("core.antipm", "pmreport", False)
-        await message.edit("PM-Report OFF!")
-    else:
-        await message.edit(
-            "Usage: "
-            f"<code>{prefix}pmreport </code>"
-            "[on|off]"
-        )
-
-
-@Client.on_message(filters.command("pmblock", prefix) & filters.me)
-async def _block(_, message: Message):
-    if len(message.command) == 1:
-        if db.get("core.antipm", "pmblock", False):
-            await message.edit(
-                "PM-Block Status: ON!\n"
-                f"Deactivated: <code>{prefix}pmblock off</code>"
-            )
-        else:
-            await message.edit(
-                "PM-Block Status: OFF!\n"
-                f"Activated: <code>{prefix}pmblock on</code>"
-            )
-    elif message.command[1] == "on":
-        db.set("core.antipm", "pmblock", True)
-        await message.edit("PM-Block ON!")
-    elif message.command[1] == "off":
-        db.set("core.antipm", "pmblock", False)
-        await message.edit("PM-Block OFF!")
-    else:
-        await message.edit(
-            "Usage: "
-            f"<code>{prefix}pmblock </code>"
-            "[on|off]"
-        )
