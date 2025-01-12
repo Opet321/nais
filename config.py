8b6fb1cfb3257b9eaf9604b8169e5b82ad62dbc2
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 00:28:50 +0700

    update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ def get_env_var(
     raise ValueError(f"Missing required configuration: {key}")
 
 
-API_ID = int(get_env_var("API_ID", "16712780"))
+API_ID = int(get_env_var("16712780"))
 API_HASH = get_env_var("API_HASH", "7941450f5313966647b6d6fde5f933dc")
 BOT_TOKEN = get_env_var("BOT_TOKEN", "7647918583:AAFRli68VM40AJJk_7-L4XZ2m5NFGDbSzU0") 
 STRING_SESSION = get_env_var("SESSION_STRING", "BQD_BEwAXPDsQTHf-r4E25RviN6QmSlpZW4LtuHhCExYvF1mqj5JMipNOJ8Qwi1MQXSASoDGNJ9bSB8gAYHuNod7T4v2z9VLnZDZtXbQcEVizCiQqPJI-Ls823CzVdv5uegHyppJ-Mmsku6DG0kmFt2kxtT57xUj2vGn_TJstzad3ocETm82Q9WLyV86UB-C4QB3KqQDwUuEBBVIXsRzUINMow4V7ojT9g9b3q2PNrr-emUcpNZGfAHy3NyjFZ_hlMjm2uYXvUgziltdgq5IdU4D-kg6XoIgRcAeQLcqoTRVqpLeNA8F0dLRfG-u3XaAfr3cquf-NaoTjCFsMhyUSO7sfahWRQAAAAFw-KFTAA")
