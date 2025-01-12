3f3b5818b414f884d951fd3b2f6b4d1a0cd9d8ce
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 00:56:34 +0700

    update feedback/base/db_client.py

--- feedback/base/db_client.py
+++ feedback/base/db_client.py
@@ -9,6 +9,30 @@ from config import MONGODB_URL
 logger = logging.getLogger("db_client")
 
 
+antipmdb = MONGODB_URL["naya"]["antipm"]
+
+async def go_antipm(user_id: int):
+    user_data = await antipmdb.users.find_one({"user_id": user_id})
+    if user_data:
+        await antipmdb.users.update_one(
+            {"user_id": user_id},
+            {"$set": {"antipm": True}},
+        )
+    else:
+        await antipmdb.users.insert_one(
+            {"user_id": user_id, "antipm": True}
+        )
+
+
+async def no_antipmk(user_id: int):
+    await antipmdb.users.delete_one({"user_id": user_id, "antipm": True})
+
+
+async def check_antipm(user_id: int):
+    user_data = await antipmdb.users.find_one({"user_id": user_id, "antipm": True})
+    return user_data
+
+
 class Database:
     def __init__(self, tg_client) -> None:
         self.client: Optional[AsyncIOMotorClient] = None
