d87a7ef0ee687d24dd18f0ab730c5d6f1017f209
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 00:32:10 +0700

    update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ def get_env_var(
 
 API_ID = int(get_env_var("API_ID"))
 API_HASH = get_env_var("API_HASH")
-BOT_TOKEN = get_env_var("BOT_TOKEN", "7647918583:AAFRli68VM40AJJk_7-L4XZ2m5NFGDbSzU0") 
-STRING_SESSION = get_env_var("SESSION_STRING", "BQD_BEwAXPDsQTHf-r4E25RviN6QmSlpZW4LtuHhCExYvF1mqj5JMipNOJ8Qwi1MQXSASoDGNJ9bSB8gAYHuNod7T4v2z9VLnZDZtXbQcEVizCiQqPJI-Ls823CzVdv5uegHyppJ-Mmsku6DG0kmFt2kxtT57xUj2vGn_TJstzad3ocETm82Q9WLyV86UB-C4QB3KqQDwUuEBBVIXsRzUINMow4V7ojT9g9b3q2PNrr-emUcpNZGfAHy3NyjFZ_hlMjm2uYXvUgziltdgq5IdU4D-kg6XoIgRcAeQLcqoTRVqpLeNA8F0dLRfG-u3XaAfr3cquf-NaoTjCFsMhyUSO7sfahWRQAAAAFw-KFTAA")
-FORUM_CHAT_ID = int(get_env_var("FORUM_CHAT_ID", "-1002314627378"))
-MONGODB_URL = get_env_var("MONGODB_URL", "mongodb+srv://apem:apem@cluster0.iraog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = get_env_var("BOT_TOKEN") 
+STRING_SESSION = get_env_var("SESSION_STRING")
+FORUM_CHAT_ID = int(get_env_var("FORUM_CHAT_ID"))
+MONGODB_URL = get_env_var("MONGODB_URL")
