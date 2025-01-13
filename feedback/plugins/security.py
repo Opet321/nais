403a4fda3e9c24620d0fbc971d9523790cab6c07
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 18:00:13 +0700

    update feedback/plugins/security.py

--- feedback/plugins/security.py
+++ feedback/plugins/security.py
@@ -10,68 +10,77 @@ from asyncio import sleep
 from feedback.base.db_client import db, pmstatus, contacts, supports
 
 
+async def is_antipm_(f, client, message):
+    user_id = client.me.id
+    antipm_c = await check_antipm(user_id)
+    if antipm_c:
+        return bool(True)
+    else:
+        return bool(False)
+
+is_antipm = filters.create(func=is_antipm_, name="is_antipm_")
+
+@Client.on_message(filters.command(["autopurge"], cmd) & filters.me)
+async def set_antipm(client, message):
+    try:
+        if len(message.command) < 2:
+            await message.reply("Please specify 'on' or 'off'.", quote=True)
+            return
+        
+        if message.command[1] == "on":
+            user_id = client.me.id
+            await go_antipm(user_id)
+            await message.reply("<b>Anti-PM activated!!</b>", quote=True)
+        elif message.command[1] == "off":
+            user_id = client.me.id
+            await no_antipmk(user_id)
+            await message.reply("<b>Anti-PM deactivated!!</b>", quote=True)
+        else:
+            kontols = await check_antipm(client.me.id)
+            kurukuru = kontols.get("antipm", "False")
+            await message.reply(f"<b>Anti-PM status:</b> <code>{kurukuru}</code>\n<b>To Activate use</b> <code>antipm on/off</code>", quote=True)
+    except Exception as e:
+        print(f"Error in set_antipm: {e}")
+        kontols = await check_antipm(client.me.id)
+        kurukuru = kontols.get("antipm", "False")
+        await message.reply(f"<b>Anti-PM status:</b> <code>{kurukuru}</code>\n<b>To Activate use</b> <code>antipm on/off</code>", quote=True)
 
 
 @Client.on_message(
-    filters.private
-    & ~filters.me
-    & ~filters.bot
-    & ~contacts
-    & ~supports
-    & pmstatus
+   ~filters.me & ~filters.bot & filters
+     .private & is_antipm 
 )
-async def _antipm_(client: Client, message: Message):
-    userpm = await client.resolve_peer(message.chat.id)
-    if db.get("core.antipm", "pmreport", False):
-        await client.invoke(
-            functions.messages.ReportSpam(
-                peer=userpm
-            )
-        )
-    if antipmdb.get("core.antipm", "pmblock", False):
-        await client.invoke(
-            functions.contacts.Block(
-                id=userpm
-            )
-        )
-    msg = await client.send_message(
-        message.chat.id,
-        "Sorry... No-PMs!"
-    )
-    for countdown in ["3", "2", "1"]:
-        await sleep(1)
-        await msg.edit(countdown)
-    await client.invoke(
-        functions.messages.DeleteHistory(
-            peer=userpm,
-            max_id=0,
-            revoke=True
-        )
-    )
+async def handle_antipm(client: Client,
+  message: Message) -> None: 
+    if message.from_user.is_contact is True:
+        return 
+    if message.from_user.is_support is True:
+        return 
+    if message.from_user.id == OWNER: 
+        return
+      
+    results = await client.get_inline_bot_results("@eyecosbot",  query="pmpermit") 
+    await client.send_inline_bot_result(message.chat.id, results.query_id, results.results[0].id)
+    peer_id = await client.resolve_peer(message.chat.id)  
+    await sleep (8)
+    await client.invoke(DeleteHistory(peer=peer_id, max_id=0, revoke=True))
 
 
-@Client.on_message(filters.command(["antipm"]))
-async def _antipm(_, message: Message):
-    if len(message.command) == 1:
-        if db.get("core.antipm", "status", False):
-            await message.edit(
-                "Anti-PM Status: ON!\n"
-                f"Deactivated: <code>{prefix}antipm off</code>"
-            )
-        else:
-            await message.edit(
-                "Anti-PM Status: OFF!\n"
-                f"Activated: <code>{prefix}antipm on</code>"
-            )
-    elif message.command[1] == "on":
-        db.set("core.antipm", "status", True)
-        await message.edit("Anti-PM ON!")
-    elif message.command[1] == "off":
-        db.set("core.antipm", "status", False)
-        await message.edit("Anti-PM OFF!")
-    else:
-        await message.edit(
-            "Usage: "
-            f"<code>{prefix}antipm </code>"
-            "[on|off]"
+
+
+@Client.on_inline_query() 
+async def handle_inline(client: Client, inline: InlineQuery) -> None: 
+    inline_query = inline.query 
+    if inline_query.strip().lower().split()[0] == "pmpermit": 
+        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="contact me", user_id=client.me.id)]] 
+        ) 
+        await inline.answer( 
+            results=[InlineQueryResultArticle(
+                id=str(uuid4()), 
+                title="Pmpermit",
+                input_message_content 
+                  =InputTextMessageContent("<b><blockquote>Maaf saya tidak bisa menerima PM, Silahkan hubungi saya melalui bot, klik tombol di bawah ini</b></blockquote>"), 
+                reply_markup=keyboard 
+            )]
         )
+
